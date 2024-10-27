295e998885fe54a86db7fa061bd1e36cdd080d5c
artyom7774 <glotij17@gmail.com>
2024-10-27 08:51:09 +0300

    ge3

--- scr/site/script.js
+++ scr/site/script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.menu-item, .submenu-item').forEach(item => {
+document.querySelectorAll('.menu-item').forEach(item => {
     item.addEventListener('click', event => {
         const content = document.querySelector('.content');
         content.innerHTML = `<p>Вы выбрали: ${event.target.innerText}</p>`;
