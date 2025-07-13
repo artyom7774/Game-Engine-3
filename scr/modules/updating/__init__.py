4cadb4059ac7a955a11a772a6dfc9cca651c44ae
Artyom <glotij17@gmail.com>
2025-07-13 22:17:08 +0300

    ge3

--- scr/modules/updating/__init__.py
+++ scr/modules/updating/__init__.py
@@ -1,11 +1,15 @@
-from ..updating import v3110to3120
+from ..updating import v3110to3120, v3120to3121
 
 versionsUpdatingTree = {
     "3.11.0": [
         "3.12.0"
+    ],
+    "3.12.0": [
+        "3.12.1"
     ]
 }
 
 versionsUpdatingFunctions = {
-    "3.11.0 -> 3.12.0": lambda name: v3110to3120.updating(name)
+    "3.11.0 -> 3.12.0": lambda name: v3110to3120.updating(name),
+    "3.12.0 -> 3.12.1": lambda name: v3120to3121.updating(name)
 }
