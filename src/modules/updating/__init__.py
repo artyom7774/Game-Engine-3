e725db13919fdd3e5eb04e0015dd7703b05a25d2
artyom7774 <Glotij17@gmail.com>
2026-03-04 09:02:08 +0300

    new updatings

--- src/modules/updating/__init__.py
+++ src/modules/updating/__init__.py
@@ -1,4 +1,4 @@
-from ..updating import v3110to3120, v3120to3121, v3121to3122, v3122to3123, v3123to3124, v3124to3125, v3125to3126, v3126to3130, v3130to3131, v3131to3132, v3132to3133, v3133to3140, v3140to3150, v3150to3151, v3151to3152, v3152to3153
+from ..updating import v3110to3120, v3120to3121, v3121to3122, v3122to3123, v3123to3124, v3124to3125, v3125to3126, v3126to3130, v3130to3131, v3131to3132, v3132to3133, v3133to3140, v3140to3150, v3150to3151, v3151to3152, v3152to3153, v3153to3160, v3160to3161
 
 versionsUpdatingTree = {
     "3.11.0": [
@@ -48,6 +48,12 @@ versionsUpdatingTree = {
     ],
     "3.15.2": [
         "3.15.3"
+    ],
+    "3.15.3": [
+        "3.16.0"
+    ],
+    "3.16.0": [
+        "3.16.1"
     ]
 }
 
@@ -67,5 +73,7 @@ versionsUpdatingFunctions = {
     "3.14.0 -> 3.15.0": lambda name: v3140to3150.updating(name),
     "3.15.0 -> 3.15.1": lambda name: v3150to3151.updating(name),
     "3.15.1 -> 3.15.2": lambda name: v3151to3152.updating(name),
-    "3.15.2 -> 3.15.3": lambda name: v3152to3153.updating(name)
+    "3.15.2 -> 3.15.3": lambda name: v3152to3153.updating(name),
+    "3.15.3 -> 3.16.0": lambda name: v3153to3160.updating(name),
+    "3.16.0 -> 3.16.1": lambda name: v3153to3160.updating(name)
 }
